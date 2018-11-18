0dee64085c977340d5eff8c3e7ec6d93ebbb188e
wafonro <wagner.fonseca.rodrigues@gmail.com>
2018-11-18 18:48:22 +0100

    Add delaunay triangulation

--- face_morph.cpp
+++ face_morph.cpp
@@ -34,16 +34,50 @@
 #include <dlib/image_processing/render_face_detections.h>
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace dlib;
 using namespace std;
+
 std::vector<cv::Point2f> landmarkToCV(full_object_detection landmarks) {
   std::vector<cv::Point2f> tmp;
   for (int i = 0; i < landmarks.num_parts(); i++) {
     point landmark = landmarks.part(i);
     tmp.push_back(cv::Point2f(landmark.x(), landmark.y()));
   }
+  cout << "tmp size : " << tmp.size() << endl;
   return tmp;
+}
+
+static void draw_delaunay(cv::Mat& img, cv::Subdiv2D& subdiv) {
+  cv::Scalar delaunay_color(255, 255, 255);
+  std::vector<cv::Vec6f> triangleList;
+  subdiv.getTriangleList(triangleList);
+  std::vector<cv::Point> pt(3);
+  cv::Size size = img.size();
+  cv::Rect rect(0, 0, size.width, size.height);
+
+  for (size_t i = 0; i < triangleList.size(); i++) {
+    cv::Vec6f t = triangleList[i];
+    pt[0] = cv::Point(cvRound(t[0]), cvRound(t[1]));
+    pt[1] = cv::Point(cvRound(t[2]), cvRound(t[3]));
+    pt[2] = cv::Point(cvRound(t[4]), cvRound(t[5]));
+
+    // Draw rectangles completely inside the image.
+    if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2])) {
+      cv::line(img, pt[0], pt[1], delaunay_color, 1, CV_AA, 0);
+      cv::line(img, pt[1], pt[2], delaunay_color, 1, CV_AA, 0);
+      cv::line(img, pt[2], pt[0], delaunay_color, 1, CV_AA, 0);
+    }
+  }
+}
+
+cv::Subdiv2D get_delaunay(std::vector<cv::Point2f>& landmarks, cv::Rect rect) {
+  cv::Subdiv2D delaunay(rect);
+  for (int i = 0; i < landmarks.size(); i++) {
+    delaunay.insert(landmarks[i]);
+  }
+  return delaunay;
 }
 
 int main() {
@@ -64,17 +98,13 @@ int main() {
     // Grab and process frames until the main window is closed by the user.
     while (!win.is_closed()) {
       // Grab a frame
-      cv::Mat temp;
-      if (!cap.read(temp)) {
+      cv::Mat img;
+      if (!cap.read(img)) {
         break;
       }
-      // Turn OpenCV's Mat into something dlib can deal with.  Note that this
-      // just wraps the Mat object, it doesn't copy anything.  So cimg is only
-      // valid as long as temp is valid.  Also don't do anything to temp that
-      // would cause it to reallocate the memory which stores the image as that
-      // will make cimg contain dangling pointers.  This basically means you
-      // shouldn't modify temp while using cimg.
-      cv_image<bgr_pixel> cimg(temp);
+      cv_image<bgr_pixel> cimg(img);
+      cv::Size size = img.size();
+      cv::Rect rect(0, 0, size.width, size.height);
 
       // Detect faces
       std::vector<rectangle> faces = detector(cimg);
@@ -82,17 +112,15 @@ int main() {
       full_object_detection face_landmarks;
       if (faces.size() > 0) {
         face_landmarks = pose_model(cimg, faces[0]);
+        auto landmarks = landmarkToCV(face_landmarks);
+        auto delaunay = get_delaunay(landmarks, rect);
+        draw_delaunay(img, delaunay);
+        // Display it all on the screen
+        win.clear_overlay();
+        win.set_image(cimg);
+        win.add_overlay(render_face_detections(face_landmarks));
+        cv::waitKey(1000);
       }
-      for (int i = 0; i < face_landmarks.num_parts(); i++) {
-        point landmark = face_landmarks.part(i);
-        cout << landmark.x() << " " << landmark.y() << endl;
-      }
-      auto landmarks = landmarkToCV(face_landmarks);
-      // Display it all on the screen
-      win.clear_overlay();
-      win.set_image(cimg);
-      win.add_overlay(render_face_detections(face_landmarks));
-      cv::waitKey(0);
     }
   } catch (serialization_error& e) {
     cout << "You need dlib's default face landmarking model file to run this "
