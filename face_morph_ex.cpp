0e453edb0758e5ca99c4a08dea3c4028aaec8620
wafonro <wagner.fonseca.rodrigues@gmail.com>
2018-11-28 18:27:17 +0100

    Seamless merge

--- face_morph_ex.cpp
+++ face_morph_ex.cpp
@@ -6,13 +6,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-
+#include "src/utils.hpp"
 using namespace cv;
 using namespace std;
 
 // Read points stored in the text files
-vector<Point2f> readPoints(string pointsFileName) {
-  vector<Point2f> points;
+std::vector<Point2f> readPoints(string pointsFileName) {
+  std::vector<Point2f> points;
   ifstream ifs(pointsFileName);
   float x, y;
   while (ifs >> x >> y) {
@@ -23,8 +23,9 @@ vector<Point2f> readPoints(string pointsFileName) {
 }
 
 // Apply affine transform calculated using srcTri and dstTri to src
-void applyAffineTransform(Mat &warpImage, Mat &src, vector<Point2f> &srcTri,
-                          vector<Point2f> &dstTri) {
+void applyAffineTransform(Mat &warpImage, Mat &src,
+                          std::vector<Point2f> &srcTri,
+                          std::vector<Point2f> &dstTri) {
   // Given a pair of triangles, find the affine transform.
   Mat warpMat = getAffineTransform(srcTri, dstTri);
 
@@ -34,7 +35,8 @@ void applyAffineTransform(Mat &warpImage, Mat &src, vector<Point2f> &srcTri,
 }
 
 // Warps and alpha blends triangular regions from img1 and img2 to img
-void morphFaces(Mat &img1, Mat &img2, vector<Point2f> &t1, vector<Point2f> &t2,
+void morphFaces(Mat &img1, Mat &img2, Mat &output, Mat &allMask,
+                std::vector<Point2f> &t1, std::vector<Point2f> &t2,
                 double alpha) {
   // Find bounding rectangle for each triangle
 
@@ -42,8 +44,8 @@ void morphFaces(Mat &img1, Mat &img2, vector<Point2f> &t1, vector<Point2f> &t2,
   Rect r2 = boundingRect(t2);
 
   // Offset points by left top corner of the respective rectangles
-  vector<Point2f> triangle1, triangle2;
-  vector<Point> triangle2Int;
+  std::vector<Point2f> triangle1, triangle2;
+  std::vector<Point> triangle2Int;
   for (int i = 0; i < 3; i++) {
     triangle1.push_back(Point2f(t1[i].x - r1.x, t1[i].y - r1.y));
     triangle2.push_back(Point2f(t2[i].x - r2.x, t2[i].y - r2.y));
@@ -52,10 +54,9 @@ void morphFaces(Mat &img1, Mat &img2, vector<Point2f> &t1, vector<Point2f> &t2,
   }
 
   // Get mask by filling triangle
-  // Mat mask = Mat::ones(r2.height, r2.width, CV_8UC1) * 256;
   Mat mask = Mat::zeros(r2.height, r2.width, CV_32FC3);
   fillConvexPoly(mask, triangle2Int, Scalar(1.0, 1.0, 1.0), 16, 0);
-  mask.convertTo(mask, CV_8UC1, 256);
+  // mask.convertTo(mask, CV_8UC1, 256);
   // Apply warpImage to small rectangular patches
   Mat img1Rect, img2Rect;
   img1(r1).copyTo(img1Rect);
@@ -70,16 +71,11 @@ void morphFaces(Mat &img1, Mat &img2, vector<Point2f> &t1, vector<Point2f> &t2,
   Mat imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2;
 
   // Copy triangular region of the rectangular patch to the output image
-  Mat output;
-  Point center = Point(r2.width / 2, r2.height / 2);
-  Mat m1, m2;
-  imgRect.convertTo(m1, CV_8UC3);
-  img2(r2).convertTo(m2, CV_8UC3);
-  seamlessClone(m1, m2, mask, center, output, NORMAL_CLONE);
-  img2(r2) = output;
-  // multiply(imgRect, mask, imgRect);
-  // multiply(img2(r2), Scalar(1.0, 1.0, 1.0) - mask, img2(r2));
-  // img2(r2) = img2(r2) + imgRect;
+
+  // img2(r2) = output;
+  multiply(imgRect, mask, imgRect);
+  output(r2) = output(r2) + imgRect;
+  allMask(r2) += allMask(r2) + mask;
 }
 
 int main(int argc, char **argv) {
@@ -87,23 +83,24 @@ int main(int argc, char **argv) {
   string filename2("mona.jpg");
 
   // alpha controls the degree of morph
-  double alpha = 0.1;
+  double alpha = 0.4;
 
   // Read input images
   Mat img1 = imread(filename1);
   Mat img2 = imread(filename2);
-
   // convert Mat to float data type
   img1.convertTo(img1, CV_32F);
   img2.convertTo(img2, CV_32F);
+  Mat output = Mat::zeros(img2.size(), CV_32FC3);
+  Mat mask = Mat::zeros(img2.size(), CV_32FC3);
 
   // empty average image
   Mat imgMorph = Mat::zeros(img1.size(), CV_32FC3);
 
   // Read points
-  vector<Point2f> points1 = readPoints(filename1 + ".txt");
-  vector<Point2f> points2 = readPoints(filename2 + ".txt");
-  vector<Point2f> points;
+  std::vector<Point2f> points1 = readPoints(filename1 + ".txt");
+  std::vector<Point2f> points2 = readPoints(filename2 + ".txt");
+  std::vector<Point2f> points;
 
   // compute weighted average point coordinates
   for (int i = 0; i < points1.size(); i++) {
@@ -118,9 +115,9 @@ int main(int argc, char **argv) {
   ifstream ifs("delaunay.txt");
   int x, y, z;
 
-  vector<Point2f> t1, t2, t;
   while (ifs >> x >> y >> z) {
     // Triangles
+    std::vector<Point2f> t1, t2, t;
 
     // Triangle corners for image 1.
     t1.push_back(points1[x]);
@@ -136,11 +133,20 @@ int main(int argc, char **argv) {
     t.push_back(points[x]);
     t.push_back(points[y]);
     t.push_back(points[z]);
-    morphFaces(img1, img2, t1, t2, alpha);
+    morphFaces(img1, img2, output, mask, t1, t2, alpha);
   }
 
-  // Display Result
-  imshow("Morphed Face", img2 / 255.0);
+  imshow("mask", mask);
+  Mat m1, m2;
+  img2.convertTo(m2, CV_8UC3);
+
+  Point2f center(center_of_points(points2));
+  Point centerInt((int)center.x, (int)center.y);
+  mask.convertTo(mask, CV_8UC1, 256);
+  output.convertTo(output, CV_8UC3);
+  seamlessClone(output, m2, mask, centerInt, output, NORMAL_CLONE);
+  imshow("Morphed Face", output);
+
   imwrite("wagnelisa.jpg", img2);
   waitKey(0);
 
