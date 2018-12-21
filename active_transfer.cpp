e6949298fe57173729580fcfa683962b42239850
ceerqingting3y <yingyu.yang@polytechnique.edu>
2018-12-21 17:03:35 +0100

    the final

--- active_transfer.cpp
+++ active_transfer.cpp
@@ -24,13 +24,16 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv){
+	//----------------------------------------------------------------------------------------------------------
+	// Part I: read target image + get webcam input frame + detect landmarks
+	//----------------------------------------------------------------------------------------------------------
 	shape_predictor pose_model;
 	deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
 	// Load face detection and pose estimation models.
 	frontal_face_detector detector = get_frontal_face_detector();
 	// read target neutral triangle information 
 	std::vector<std::vector<int>> triangles = read_triangles("jpg/baseline.txt");
-	//try {
+	try {
 		VideoCapture cap(0);
 		if (!cap.isOpened()) {
 		cerr << "Unable to connect to camera" << endl;
@@ -46,12 +49,7 @@ int main(int argc, char** argv){
 		}
 		imshow("Real face",source0);
 		//waitKey(30);
-		
 
-
-		//------------------------------------------------------------------------------------------------------
-		//read target image
-		//------------------------------------------------------------------------------------------------------
 		// read target image t (neutral)
 		string filename_t = argv[1];
 		cv::Mat target0 = cv::imread(filename_t);
@@ -69,15 +67,9 @@ int main(int argc, char** argv){
 		tface_landmarks = pose_model(img_t, tfaces[0]);
 		auto tpoints = vectorize_landmarks(tface_landmarks);
 		Rect rect_face_t = boundingRect(tpoints);
-		// save only the face part 
+		// use only the face part 
 		Mat face_t;
 		target0(rect_face_t).copyTo(face_t);
-	
-	// read source image s (non-neutral)
-    //string filename_s = argv[1];
-	//cv::Mat source0 = cv::imread(filename_s);
-    //Mat source0;
-    //resize(source00,source0,cv::Size(source00.cols*5,source00.cols*5));
 		cv_image<bgr_pixel> img_s(source0);
 		cv::Mat source;
 		source0.convertTo(source, CV_32FC3);
@@ -98,11 +90,14 @@ int main(int argc, char** argv){
 		// try s1 to t1 transformation (show image )
 		face_t.convertTo(face_t,CV_32FC3);
 		face_s.convertTo(face_s,CV_32FC3);
-	
-		// face_t2 transfer face_t to face_s (get the emotional change of face_t)
-		//Mat face_t2 = Mat::zeros(face_s.rows*5,face_s.cols*5, CV_32FC3);
-		Mat face_t2 = Mat::zeros(face_s.rows,face_s.cols, CV_32FC3);
 
+		//----------------------------------------------------------------------------------------------------------
+		// Part II: warp target face to source face in order to get the expression of the source face 
+		//          and get some morphing: eg. copy the mouth part to the target image 
+		//----------------------------------------------------------------------------------------------------------
+		Mat face_t2 = Mat::zeros(face_s.rows,face_s.cols, CV_32FC3);
+		// we use delaunay triangles to operate affine transform 
+		// we work for every triangle
 		for (std::vector<std::vector<int>>::iterator it = triangles.begin(); it!=triangles.end();++it){
 			std::vector<Point2f> sr, tr;
 			int a,b,c;
@@ -112,14 +107,11 @@ int main(int argc, char** argv){
 			sr.push_back(spoints[a]);
 			sr.push_back(spoints[b]);
 			sr.push_back(spoints[c]);
-			
 			tr.push_back(tpoints[a]);
 			tr.push_back(tpoints[b]);
 			tr.push_back(tpoints[c]);
-		
 			Rect rs1 = boundingRect(sr);
 			Rect rt1 = boundingRect(tr);
-			
 			std::vector<Point2f> triangle1,triangle2;
 			//turn the points to the coordinates in the cropped face image 
 			for (int i = 0; i < 3; i++) {
@@ -128,17 +120,13 @@ int main(int argc, char** argv){
 				//target image points 
 				triangle2.push_back(Point2f(tr[i].x - rect_face_t.x, tr[i].y - rect_face_t.y));
 			}
-
 			transfer_to_source(triangle2,triangle1,face_t,face_t2);
 		}
 		Mat t2;
 		face_t2.convertTo(t2, CV_8UC3);
-		//imshow("Morphed Face2", t2);
-		//display it all on the screen
-		//waitKey(0);
 		
+
 		//motify mouth 
-		/*
 		int mouth_contours[] = {60,61,62,63,64,65,67};
 		std::vector<Point2f> mouth_faces;
 		std::vector<Point> mouth_int;
@@ -153,21 +141,14 @@ int main(int argc, char** argv){
 		multiply(face_s,mouth_mask,mouth_src);
 		multiply(face_t2,Scalar(1.0,1.0,1.0)-mouth_mask,mouth_dst);
 		new_t2 = mouth_src + mouth_dst;
-		*/
-		Mat new_t2;
-		new_t2 = face_t2.clone();
-
-
-		//-----------------------------------------------------------------------------------------------------------------------------
-		// warp image back to its face
-		//-----------------------------------------------------------------------------------------------------------------------------
-		// version1
 		
+		//----------------------------------------------------------------------------------------------------------
+		// Part III: warp image back to its face
+		//----------------------------------------------------------------------------------------------------------
+		// warp image back to its face
 		int face_contours[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,26,25,24,23,20,19,18,17};
 		int month_contours[] = {60,61,62,63,64,65,67};
 		int elbows[] = {17,18,19,20,21,22,23,24,25,26};
-		
-
 		cv_image<bgr_pixel> img_t2(t2);
 
 		// Detect target
@@ -178,7 +159,7 @@ int main(int argc, char** argv){
 		Rect rect_face_t2 = boundingRect(t2points);
 		//show_landmarks(t2, t2points);
 
-
+		//get the face contour points for source face and target face to get the 
 		std::vector<Point2f> contours_facet;
 		std::vector<Point2f> contours_faces;
 		std::vector<Point2f> contours_facet2;
@@ -195,19 +176,11 @@ int main(int argc, char** argv){
 		center.x = center.x/25;
 		center.y = center.y/25;
 		
-		
-		//show_landmarks(new_t2, contours_facet2);
-		
 		// triangulation
 		// use the target face to get triangles 
 		std::vector<std::vector<int>> triangles_contours = get_triangles_from_target(target0, contours_facet, "face_contour_");
-		//show_delauney(target0, contours_facet, triangles_contours);
-		//std::vector<std::vector<int>> triangles_contours = read_triangles("jpg/face_div.txt");
-		//show_delauney(target0, tpoints, triangles_contours);
-
-		Mat face_tnext = target.clone();//Mat::zeros(target0.rows,target0.cols,CV_32FC3);//target.clone();//Mat::zeros(target0.rows,target0.cols,CV_32FC3);////
-		//Mat face_tback = target.clone();
-
+		
+		Mat face_tnext = target.clone();
 		for (std::vector<std::vector<int>>::iterator it = triangles_contours.begin(); it!=triangles_contours.end();++it){
 			std::vector<Point2f> sr, tr;
 			int a,b,c;
@@ -217,17 +190,10 @@ int main(int argc, char** argv){
 			sr.push_back(contours_faces[a]);
 			sr.push_back(contours_faces[b]);
 			sr.push_back(contours_faces[c]);
-			
-			
 			tr.push_back(contours_facet[a]);
 			tr.push_back(contours_facet[b]);
 			tr.push_back(contours_facet[c]);
 			
-		/*
-			tr.push_back(tpoints[a]);
-			tr.push_back(tpoints[b]);
-			tr.push_back(tpoints[c]);
-			*/
 			Rect rs1 = boundingRect(sr);
 			Rect rt1 = boundingRect(tr);
 			
@@ -235,40 +201,7 @@ int main(int argc, char** argv){
 		}
 		Mat face_tnext2;
 		linear_face_contours(contours_facet, face_tnext,target,face_tnext2);
-		/*
-		cout<<"mask"<<endl;
-		Mat mask_tnext = Mat::zeros(target0.rows,target0.cols,CV_32FC3);
-		std::vector<std::vector<Point>> contours;
-		cout<<"findcontours"<<endl;
-		Mat temp;
-		cvtColor(face_tnext,temp,CV_RGB2GRAY);
-		temp.convertTo(temp,CV_8UC1);
 		
-		findContours(temp,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-		fillConvexPoly(mask_tnext, contours[0], Scalar(1.0, 1.0, 1.0), 16, 0);
-		cout<<"change face2"<<endl;
-		Mat face_tnext2 = target.clone();
-		cout<<"multiply face & mask"<<endl;
-		multiply(face_tnext2,Scalar(1.0,1.0,1.0)-mask_tnext,face_tnext2);
-		cout<<"add face & mask"<<endl;
-		face_tnext2 = face_tnext2 + face_tnext;
-	*/	
-		/*
-		cout<<"fill holes"<<endl;
-		for(int i=max(0,rect_face_t.x-100);i<min(rect_face_t.x + rect_face_t.width+50,target.cols);i++){
-			for(int j=max(0,rect_face_t.y-100);j<min(rect_face_t.y+rect_face_t.height+50,target.rows);j++){
-				if(face_tnext.at<Vec3f>(i,j)[0] <=10 && face_tnext.at<Vec3f>(i,j)[1] <=10 && face_tnext.at<Vec3f>(i,j)[2] <=10){
-					face_tnext.at<Vec3f>(i,j)[0] = target.at<Vec3f>(i,j)[0];
-					face_tnext.at<Vec3f>(i,j)[1] = target.at<Vec3f>(i,j)[1];
-					face_tnext.at<Vec3f>(i,j)[2] = target.at<Vec3f>(i,j)[2];
-				}
-				
-			}
-		}
-		*/
-		
-		
-
 		Mat t_next;
 		face_tnext2.convertTo(t_next, CV_8UC3);
 		
@@ -280,64 +213,7 @@ int main(int argc, char** argv){
 			break;
 		}
 	}
-		/*
-		Mat normal_clone;
-		face_tnext.convertTo(face_tnext, CV_8UC3);
-		face_tback.convertTo(face_tback,CV_8UC3);
-		mask_tnext.convertTo(mask_tnext,CV_8UC1, 256);
-		cout<<"clone!"<<endl;
-		cout<<face_tnext.rows<<" "<<face_tnext.cols<<endl;
-		cout<<face_tback.rows<<" "<<face_tback.cols<<endl;
-		cout<<mask_tnext.rows<<" "<<face_tnext.cols<<endl;
-		*/
-		//seamlessClone(face_tnext, face_tback, mask_tnext, center, normal_clone, NORMAL_CLONE);
-
-		
-		
-		
-
-
-    /*
-    // version2
-    // choose 2 eyes and nose to calculate the transform from source to original 
-    
-    
-
-    int points_3[] = {36,45};
-
-    cout<<"CHANGE FACE!!!!"<<endl;
-    cout<<"CHANGE FACE!!!!"<<endl;
-    cout<<"CHANGE FACE!!!!"<<endl;
-    cout<<"CHANGE FACE!!!!"<<endl;
-    cout<<"CHANGE FACE!!!!"<<endl;
-    std::vector<Point2f> t_3points;
-    std::vector<Point2f> s_3points;
-    for (int i=0;i<2;i++){
-        t_3points.push_back(tpoints[points_3[i]]);
-        s_3points.push_back(t2points[points_3[i]]);//Point2f(t2points[points_3[i]].x - rect_face_s.x,t2points[points_3[i]].y - rect_face_s.y));
-    }
-    Mat face_tnext2 = target.clone();
-    //Mat face_tnext2 = Mat::zeros(target.rows,target.cols,target.type());
-    //Mat trans = cv::estimateRigidTransform(s_3points,t_3points,false);
-    
-    cout<<"FUNCTION!!!!"<<endl;
-    Mat warp;
-    similarityTransform(s_3points,t_3points,warp);
-    //warpAffine(face_t2,face_tnext2,warp,face_tnext2.size());
-    change_face(contours_faces,contours_facet,face_t2,face_tnext2,s_3points,t_3points);
-
-    //change_face(contours_facet,contours_faces,face_tnext2,face_t2,t_3points,s_3points);
-    
-
-    Mat t_next2;
-	face_tnext2.convertTo(t_next2, CV_8UC3);
-
-    imshow("Morphed Face4", t_next2);
-	//display it all on the screen
-	waitKey(0);
-	*/
-
-//	}catch(...){
-//		cout<<"Something went wrong! Please try again!"<<endl;
-//	}
+	}catch(...){
+		cout<<"Something went wrong! Please try again!"<<endl;
+	}
 }
